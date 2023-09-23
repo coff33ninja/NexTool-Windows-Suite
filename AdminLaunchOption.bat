b235b0bf7f9813c0ab60d48e8dc7b92783353012
Dragohn <coff33ninja69@gmail.com>
2023-09-24 01:48:03 +0200

    Update AdminLaunchOption.bat

--- AdminLaunchOption.bat
+++ AdminLaunchOption.bat
@@ -37,6 +37,84 @@ setlocal & pushd .
 cd /d %~dp0
 if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 
+rem ---- Start of Install/Update section ----
+
+rem Create the NexTool directory if it doesn't exist
+IF NOT EXIST C:\NexTool mkdir C:\NexTool
+icacls C:\NexTool /grant Everyone:F
+
+rem Create the PS directory if it doesn't exist
+IF NOT EXIST C:\PS mkdir C:\PS
+icacls C:\PS /grant Everyone:F
+
+rem Ensure Chocolatey is installed or updated
+choco -v || (
+    @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+)
+rem Install or update packages
+choco install choco -y || choco upgrade choco -y
+choco install aria2 -y || choco upgrade aria2 -y
+choco install wget -y || choco upgrade wget -y
+choco install curl -y || choco upgrade curl -y
+
+rem Check if winget is installed
+winget --version >NUL 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+   echo Winget is not detected, attempting installation...
+
+   echo [Method 1] Trying to install using latest release link from GitHub...
+   powershell -Command "Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile 'C:\PS\WinGet.msixbundle'"
+   powershell -Command "Add-AppxPackage 'C:\PS\WinGet.msixbundle'"
+
+   winget --version >NUL 2>&1
+   IF %ERRORLEVEL% NEQ 0 (
+      echo [Method 2] Trying to install using a specific version from GitHub...
+      powershell -Command "Invoke-WebRequest -Uri 'https://github.com/microsoft/winget-cli/releases/download/v1.5.2201/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile 'C:\PS\WinGet.msixbundle'"
+      powershell -Command "Add-AppxPackage 'C:\PS\WinGet.msixbundle'"
+   )
+
+   winget --version >NUL 2>&1
+   IF %ERRORLEVEL% NEQ 0 (
+      echo [Method 3] Please manually install "App Installer" from the Microsoft Store.
+      echo Opening the Microsoft Store page for "App Installer"...
+      start "https://apps.microsoft.com/store/detail/app-installer/9NBLGGH4NNS1"
+      pause
+      exit
+   )
+)
+cls
+
+for /f "delims=" %%i in ('winget --version') do set WINGET_VERSION=%%i
+echo Winget version: %WINGET_VERSION%
+
+for /f "delims=" %%i in ('choco --version 2^>^&1') do set CHOCO_VERSION=%%i
+echo Chocolatey version: %CHOCO_VERSION%
+
+for /f "delims=" %%i in ('aria2c --version ^| findstr "aria2 version"') do set ARIA2C_VERSION=%%i
+echo %ARIA2C_VERSION%
+
+for /f "delims=" %%i in ('wget --version ^| findstr "GNU Wget"') do set WGET_VERSION=%%i
+echo %WGET_VERSION%
+
+for /f "delims=" %%i in ('curl --version ^| findstr "curl"') do set CURL_VERSION=%%i
+echo %CURL_VERSION%
+
+echo Packagemanagers are up to date!
+
+pause
+rem ---- End of Install/Update section ----
+
 cls
 python NexTool.py
+
+echo Clearing out C:\NexTool...
+del /Q C:\NexTool\*.*
+for /D %%i in (C:\NexTool\*) do rmdir /S /Q "%%i"
+rmdir /S /Q C:\NexTool
+
+echo Clearing out C:\PS...
+del /Q C:\PS\*.*
+for /D %%i in (C:\PS\*) do rmdir /S /Q "%%i"
+rmdir /S /Q C:\PS
+
 pause
